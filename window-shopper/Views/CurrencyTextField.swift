9f71bcc87997c0c31a84c5a01133afe63a108b19
Alexander Kufel <kufelalexander@gmail.com>
2018-07-21 16:42:06 +0300

    Added currency Label

--- window-shopper/Views/CurrencyTextField.swift
+++ window-shopper/Views/CurrencyTextField.swift
@@ -11,6 +11,20 @@ import UIKit
 @IBDesignable
 class CurrencyTextField: UITextField {
 
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLabel = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLabel.backgroundColor = #colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 0.5106217894)
+        currencyLabel.textAlignment = .center
+        currencyLabel.textColor = #colorLiteral(red: 0.3004557192, green: 0.3004557192, blue: 0.3004557192, alpha: 1)
+        currencyLabel.layer.cornerRadius = 5.0
+        currencyLabel.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLabel.text = formatter.currencySymbol
+        addSubview(currencyLabel)
+    }
     
     override func prepareForInterfaceBuilder() {
         customizeView()
@@ -25,6 +39,7 @@ class CurrencyTextField: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        clipsToBounds = true
         
         if let placeholder = placeholder {
             let place = NSAttributedString(string: placeholder, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.5)])
